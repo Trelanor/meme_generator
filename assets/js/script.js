1555bb70c14084fec976f1db5aadc5cd87673073
psaulay <36258677+psaulay@users.noreply.github.com>
2018-06-13 09:32:53 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 function live_view(){
-    var def = "zone-texte1";
+    var def1 = "zone-texte1";
+    var def2 = "zone-texte2";
     var first = document.getElementById("first_paragraphe").value;
     var second = document.getElementById("second_paragraphe").value;
-    if (first.length > 0){
+
+    if (first.length == 0 && second.length == 0){
+        document.getElementById('first').innerHTML = def1;
+        document.getElementById('second').innerHTML = def2;
+    }else if (first.length > 0 && second.length == 0){
         document.getElementById('first').innerHTML = first;
+        document.getElementById('second').innerHTML = def2;
+    }else if (first.length == 0 && second.length > 0){
+        document.getElementById('first').innerHTML = def1;
         document.getElementById('second').innerHTML = second;
-    } else {
-        document.getElementById('first').innerHTML = def;
+    }else{
+        document.getElementById('first').innerHTML = first;
         document.getElementById('second').innerHTML = second;
     }
 }
